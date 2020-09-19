a3aa12fef52f96b6e446e61670c21bd1583330b6
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-09-19 18:41:06 +0530

    Update instances.tf

--- src/webapp/modules/instances/instances.tf
+++ src/webapp/modules/instances/instances.tf
@@ -24,11 +24,11 @@ key_name = "virginia"
 user_data = "${data.template_file.webserver-userdata.rendered}"
 tags = "${map("Name", format("web-server-%d", count.index + 1))}"
 
-#root_block_device {
- # "volume_type" = standard
-  #"volume_size" = 15
-  #"delete_on_termination" = true
-  #}
+root_block_device {
+  "volume_type" = "standard"
+  "volume_size" = "15"
+  "delete_on_termination" = "true"
+  }
 
   }
 
