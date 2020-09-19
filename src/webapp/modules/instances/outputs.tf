169af030dacabfaf36cbd41cb572fd866aa5064d
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-09-19 18:33:20 +0530

    Update outputs.tf

--- src/webapp/modules/instances/outputs.tf
+++ src/webapp/modules/instances/outputs.tf
@@ -1,6 +1,6 @@
-output "webserver_publicip" {
-value = "${aws_instance.webserver.public_ip}"
-}
+#output "webserver_publicip" {
+#value = "${aws_instance.webserver.public_ip}"
+#}
 #output "appserver_publicip" {
 #value = "${aws_instance.appserver.public_ip}"
 #}
