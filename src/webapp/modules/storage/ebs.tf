505e1d452964b616dcbec7b9d97a0e6df0be4d5d
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-09-19 22:57:45 +0530

    Update ebs.tf

--- src/webapp/modules/storage/ebs.tf
+++ src/webapp/modules/storage/ebs.tf
@@ -6,5 +6,5 @@ availability_zone = "us-east-1c"
 resource "aws_volume_attachment" "vattach"{
 device_name = "/dev/xvdf"
 volume_id = "${aws_ebs_volume.ebs3.id}"
-instance_id = "${aws_instance.webserver[1].id}"
+instance_id = "${aws_instance.webserver.id}"
 }
