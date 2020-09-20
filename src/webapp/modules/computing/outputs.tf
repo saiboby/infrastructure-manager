d72e2dea8ce87f61bb4d4e5b8bc24f8527749571
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-09-20 23:36:47 +0530

    Update outputs.tf

--- src/webapp/modules/computing/outputs.tf
+++ src/webapp/modules/computing/outputs.tf
@@ -1,6 +1,9 @@
 output "webserver" {
 value = "${aws_instance.webserver[0].id}"
 }
+output "dbserver" {
+value = "${aws_instance.dbserver[0].id}"
+}
 #output "appserver_publicip" {
 #value = "${aws_instance.appserver.public_ip}"
 #}
